2af60b3bdb0a26eb5d0079803a70e1c983c47fce
Evan Michael Rayburn <evanmrayburn@gmail.com>
2026-08-24 06:00:21 -0700

    created repository and Hello.cpp

---
+++ Lab1/hello.cpp
@@ -0,0 +1,13 @@
+/*** 
+ * I'm making a github repository for this class!
+***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
