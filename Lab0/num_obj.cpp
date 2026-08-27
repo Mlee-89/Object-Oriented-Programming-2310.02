38933d1b63a2cb96ca73c073e42426bd4041f4b1
Mlee-89 <evanmrayburn@gmail.com>
2026-08-27 10:19:59 -0700

    went over arrays

--- Lab0/num_obj.cpp
+++ Lab0/num_obj.cpp
@@ -12,11 +12,18 @@ int main() {
     char char_example = '1';
     bool bool_example = true;
 
+    //arry example
+    const int SIZE = 10;
+    int arr[SIZE];
+
     //8 Bits = 1 Byte
     cout << "int_example: " << sizeof(int_example) << endl;
     cout << "float_example: " << sizeof(float_example) << endl;
     cout << "double_example: " << sizeof(double_example) << endl;
     cout << "char_example: " << sizeof(char_example) << endl;
     cout << "bool_example: " << sizeof(bool_example) << endl;
+    cout << "arr_int_example: " << sizeof(arr) << endl;
+    cout << "arr(0): " << arr[0] << endl;
+    cout << "arr(1): " << arr[1] << endl;
     return 0;
 }
